b75e751515036f3b6cbee094b6dea2fdc04dda67
Alan Hou <alanhou911222@gmail.com>
2022-05-17 14:41:31 -0400

    pagination set up

--- feed.js
+++ feed.js
@@ -1,23 +1,44 @@
 $(function() { // Document ready function
-    var url = 'https://events.umich.edu/week/json?v=2' // This is the URL for your JSON feed. In this example it will pull all of todays events.
+    var url = 'https://events.umich.edu/list/json?filter=all&range=2022-05-16to2022-05-18&v=2' // This is the URL for your JSON feed. In this example it will pull all of todays events.
     
+    //pagination
+    var state = {
+        'page':1,
+        'elements': 8,
+        'count': 0,
+        'pageNum': 0
+    }
+    
+    //copy of events data
+    var events;
+
+    function pagination(data, page){
+        let trimStart = (page - 1) * state.elements;
+        let trimEnd = trimStart + state.elements;
+        let trimmedData = data.slice(trimStart, trimEnd);
+        for(i in trimmedData) { // loop though list of objects
+            if(i%4==0){
+                row = '<div class="eventRow">';
+                $('#myfeed').append(row);
+            }
+            html = buildEvent(trimmedData[i]); // build html for object
+            $('.eventRow').last().append(html); // append each object to the <div id="myfeed"></div>
+        }
+    }
+
     // Run an ajax call. The documentation is here : http://api.jquery.com/jquery.ajax/
     $.ajax({
         url: url, // Set the URL for the json feed
         success: function(data) { // Run this if there is a successful call
-            if(data.length){ // if anything was returned
-                for(i in data) { // loop though list of objects
-                    if(i%4==0){
-                        row = '<div class="eventRow">';
-                        $('#myfeed').append(row);
-                    }
-                    html = buildEvent(data[i]); // build html for object
-                    $('.eventRow').last().append(html); // append each object to the <div id="myfeed"></div>
-                }
+            events = data;
+            state.count = events.length;
+            state.pageNum = Math.ceil(state.count/state.elements);
+            if(events.length){ // if anything was returned
+                pagination(events,state.page);
             }
         }
     });
-
+    
     // create html for object.
     function buildEvent(obj) {
         html = '<div class="event">';
@@ -43,4 +64,5 @@ $(function() { // Document ready function
 
         return html;
     }
+    
 });
